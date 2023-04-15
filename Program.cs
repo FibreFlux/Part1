c102eb7255d1ab50b3fda8ff7f48fbffff423ee8
FibreFlux <tmamabolo4@gmail.com>
2023-04-15 16:48:12 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10058057_PROG6221_PortfolioOfEvidencePart1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
