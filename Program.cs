6f54152396dae786f707482e7ad03769d9de4f88
FibreFlux <tmamabolo4@gmail.com>
2023-04-15 17:11:21 +0200

    Added command line statements for the number of ingrediants, name of the ingrediants, quantity as well as the unit of measurement

--- Program.cs
+++ Program.cs
@@ -10,6 +10,20 @@ namespace ST10058057_PROG6221_PortfolioOfEvidencePart1
     {
         static void Main(string[] args)
         {
+            int ingCount = 1;
+            Console.WriteLine("Please enter how much ingrediants for your recipe");
+            int noOfIngrediants = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < noOfIngrediants; i++)
+            {
+                Console.WriteLine("Please enter the name of the ingrediant");
+                string nameOfIngrediant = Console.ReadLine();
+                Console.WriteLine("Please enter how much " + nameOfIngrediant + " you need to add to the recipe");
+                int quantity = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the ingredaint's unit of measurement");
+                string unitOfMeasurement = Console.ReadLine();
+                Console.WriteLine("Ingrediant No. " + ingCount + "\nName: " + nameOfIngrediant + "\nQuanitity: " + quantity + "\nUnit of Measurement: " + unitOfMeasurement);
+                ingCount++;
+            }
 
         }
     }
