2d7016ad7b10bdfc2299606ee710ce3cd3480212
FibreFlux <tmamabolo4@gmail.com>
2023-04-18 16:38:03 +0200

    I have made a global array called "recipeArr" which will store the ingrediants, their quantity and unit of measurement for a recipe, i have also made the "nameOfIngrediants" , "quantity", "unitOfMeasurement", and "ingCount" as well as a newly created "recipe" string variable that stores the name of the recipe that the user wants to make. i have added a foreach loop for displaying the contents of the array

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,23 +11,41 @@ namespace ST10058057_PROG6221_PortfolioOfEvidencePart1
 {
     internal class Program
     {
+        public static string[] recipeArr = new string[50];
+        
+
         static void Main(string[] args)
         {
-            int ingCount = 1;
+            int ingCount = 0;
+            string nameOfIngrediant;
+            int quantity;
+            string unitOfMeasurement;
+            string recipe;
+            Console.WriteLine("Please enter the recipe that you would like to make");
+            recipe = Console.ReadLine();
             Console.WriteLine("Please enter how much ingrediants for your recipe");
             int noOfIngrediants = Convert.ToInt32(Console.ReadLine());
             for (int i = 0; i < noOfIngrediants; i++)
             {
                 Console.WriteLine("Please enter the name of the ingrediant");
-                string nameOfIngrediant = Console.ReadLine();
+                nameOfIngrediant = Console.ReadLine();
                 Console.WriteLine("Please enter how much " + nameOfIngrediant + " you need to add to the recipe");
-                int quantity = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Please enter the ingredaint's unit of measurement");
-                string unitOfMeasurement = Console.ReadLine();
-                Console.WriteLine("Ingrediant No. " + ingCount + "\nName: " + nameOfIngrediant + "\nQuanitity: " + quantity + "\nUnit of Measurement: " + unitOfMeasurement);
+                quantity = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the ingrediant's unit of measurement");
+                unitOfMeasurement = Console.ReadLine();
+                recipeArr[i] = "Ingrediant: " + nameOfIngrediant + "\nQuantity: " + quantity + " " + unitOfMeasurement + "\n";
                 ingCount++;
+                
             }
 
+            foreach (var recipes in recipeArr)
+            {
+                if (recipes != null)
+                    Console.WriteLine(recipes);
+            }
+           
         }
+
+        
     }
 }
